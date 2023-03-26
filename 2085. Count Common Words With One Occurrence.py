34075c2ed934c3974e51fae589f0bd5751cdf637
CHITTAJALLU   LALITHA KALYAN <77020331+kalyan0309@users.noreply.github.com>
2023-03-26 16:50:37 +0530

    Create 2085. Count Common Words With One Occurrence.py

---
+++ 2085. Count Common Words With One Occurrence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countWords(self, words1: List[str], words2: List[str]) -> int:
+        count=0
+        for i in words1:
+            if words1.count(i) == words2.count(i) and words1.count(i)==1 and words2.count(i)==1:
+                count+=1
+            else:
+                continue
+        return count
+
+        
